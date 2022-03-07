e05e79117de7f3b06be7964a05d241cbf5551afa
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-07 20:07:25 +0200

    add number of values in data test

--- tests/test_coingecko_scraper.py
+++ tests/test_coingecko_scraper.py
@@ -1,15 +1,19 @@
 import unittest
-from scrapers.scrape_coingecko import scrape_coingecko
+from scrapers.scrape_coingecko import get_data
 
-class TestScraper(unittest.TestCase):
+class TestCoinGeckoScraper(unittest.TestCase):
 
     def test_data_not_empty(self):
         data = get_data()
         self.assertTrue(data)
 
-    def test_datavalues(self):
+    def test_coin_data_amount(self):
         data = get_data()
-        self.assertEqual(4, data.length())
+        self.assertEqual(50, len(data))
+
+    def test_numof_data_values(self):
+        data = get_data()
+        self.assertEqual(4, len(data[0]))
 
 if __name__ == '__main__':
     unittest.main()
