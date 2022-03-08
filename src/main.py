7cb58bf6d602e0449b36df8a60fd9365b7bb64c8
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-08 21:32:22 +0200

    add coinlib scraper

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import scrapers.scrape_coingecko
+import scrapers.scrape_coinlib
 
 if __name__ == "__main__":
     coingecko_data = scrapers.scrape_coingecko.get_data()
-
-    print(coingecko_data)
+    scrapers.scrape_coinlib.get_data()
     
