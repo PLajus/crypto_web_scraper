94a972e6b257939935d75f491dc7ee26891541e4
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-08 21:23:38 +0200

    add coinlib scraper tests

---
+++ tests/test_coinlib_scraper.py
@@ -0,0 +1,19 @@
+import unittest
+from scrapers.scrape_coinlib import get_data
+
+class TestCoinLibScraper(unittest.TestCase):
+
+    def test_data_not_empty(self):
+        data = get_data()
+        self.assertTrue(data)
+
+    def test_coin_data_amount(self):
+        data = get_data()
+        self.assertEqual(50, len(data))
+
+    def test_numof_data_values(self):
+        data = get_data()
+        self.assertEqual(4, len(data[0]))
+
+if __name__ == '__main__':
+    unittest.main()
