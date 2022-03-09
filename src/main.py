9da788f4aa036a59664edf5c1ebd1b380f9a59c5
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-09 13:40:14 +0200

    Add data updates every 10 seconds

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+import time
+
 import scrapers.scrape_coingecko
 import scrapers.scrape_coinlib
 
 if __name__ == "__main__":
-    coingecko_data = scrapers.scrape_coingecko.get_data()
-    scrapers.scrape_coinlib.get_data()
+
+    while True:
+        data = scrapers.scrape_coingecko.get_data()
+
+        if data is None:
+            data = scrapers.scrape_coinlib.get_data()
+
+        time.sleep(10)
+
+    # TODO: push data to DB
     
