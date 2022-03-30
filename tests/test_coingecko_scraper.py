679bbedc96f507203f4024b447bdba267c482422
Povilas Lajus <povilas.lajus@gmail.com>
2022-03-30 15:41:33 +0300

    refactor: move data arrange to setup

--- tests/test_coingecko_scraper.py
+++ tests/test_coingecko_scraper.py
@@ -3,17 +3,17 @@ from scrapers.scrape_coingecko import get_data
 
 class TestCoinGeckoScraper(unittest.TestCase):
 
+    def setUp(self):
+        self.coins = get_data()
+
     def test_data_not_empty(self):
-        data = get_data()
-        self.assertTrue(data)
+        self.assertTrue(self.coins)
 
     def test_coin_data_amount(self):
-        data = get_data()
-        self.assertEqual(50, len(data))
+        self.assertEqual(50, len(self.coins))
 
     def test_numof_data_values(self):
-        data = get_data()
-        self.assertEqual(4, len(data[0]))
+        self.assertEqual(4, len(self.coins[0]))
 
 if __name__ == '__main__':
     unittest.main()
